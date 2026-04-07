a67167ef27d89383a9ff671248cd883b1c454306
eiltekmm1 <eiltekmm1@gmail.com>
2026-04-07 12:12:18 +0900

    260407 no.1

--- vite.config.js
+++ vite.config.js
@@ -9,17 +9,18 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] })
   ],
   server: {
+    port: 80, 
     proxy: {
       '/api': {
-        target: 'http://localhost:80',
+        target: 'http://localhost:8080', 
         changeOrigin: true
-     },
-      // 🌟 이 부분이 꼭 추가되어야 채팅이 백엔드로 넘어갑니다!
+      },
       '/ws-chat': {
         target: 'http://localhost:8080',
-        ws: true, // 웹소켓 허용
+        ws: true,
         changeOrigin: true
       }
     }
   }
-});
+}
+);
